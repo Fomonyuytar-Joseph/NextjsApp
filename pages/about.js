71f4a479c320e1d594eb71ac46aa978eed90cf01
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-08-23 18:53:37 +0100

    add images and meta data

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import Head from 'next/head'
 
 function About() {
   return (
+   <>
+   <Head>
+    <title>Customer List | About</title>
+    <meta name="keywords" content="customers"/>
+    </Head>
     <div>
         <h1>about</h1>
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in ex eu lorem malesuada auctor in fring</p>
     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in ex eu lorem malesuada auctor in fring </p>
     </div>
+   </>
   )
 }
  
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Link from "next/link";
 import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 
 export default function Home() {
   return (
+   <>
+   <Head>
+    <title>Customer List | Home</title>
+    <meta name="keywords" content="customers"/>
+    </Head>
     <div >
       <h1 className={styles.title}>Homepage</h1>
       <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
@@ -11,6 +17,6 @@ export default function Home() {
       <Link href="/ninjas/">
         <a className={styles.btn}>See Customer Listing</a>
       </Link>
-    </div>
+    </div></>
   );
 }
