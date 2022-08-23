caf2e9494e2b6cef9f620bf9d7791a2bbc0d73cd
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-08-23 17:37:36 +0100

    create custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+function NotFound() {
+  return (
+    <div className="not-found">
+       <h1>Oooooops....</h1>
+       <h2>The page cannot be found</h2>
+       <p>Go back to the <Link href="/ ">Home Page</Link></p>
+       
+        </div>
+  )
+}
+
+export default NotFound
